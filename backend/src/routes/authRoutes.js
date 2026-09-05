c498fcac8612105046231a887353aecefacb62f8
duttaanirban <duttaanirbanad@gmail.com>
2026-09-05 10:10:34 +0530

    middleware added

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authController");
+const {
+  signup,
+  login,
+} = require("../controllers/authController");
+
+const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/me", protect, (req, res) => {
+  res.json({
+    message: "Authentication successful",
+    user: req.user,
+  });
+});
 
 module.exports = router;
